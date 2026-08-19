0459dddd4afeecdabee81361a3087f360725f37c
Dip <dipkarmokar21@gmail.com>
2026-08-19 11:43:18 +0600

    Clean repository and update gitignore

--- app/build/generated/hilt/component_trees/release/dagger/hilt/internal/processedrootsentinel/codegen/_com_dip_selfprotocol_SelfProtocolApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.dip.selfprotocol.SelfProtocolApp"
-)
-public final class _com_dip_selfprotocol_SelfProtocolApp {
-}
